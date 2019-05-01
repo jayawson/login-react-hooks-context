ea5fd9bb7d7d71de8ae857f99b8a2daf073fa94c
Jorge Marcial Garcia Rizo <jorge.garcia@sintec.com>
2019-05-01 12:56:52 -0500

    Finish demo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CheckAuth from './CheckAuth';
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return <CheckAuth />;
 }
 
 export default App;
---
+++ src/containers/Page1/Page1.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+
+function Page1() {
+	return (
+		<Typography>Hello from Page 1</Typography>
+	);
+}
+
+export default Page1;
